5131fd3a038b1503a474149c65cb71d29d73a96c
Nick Borromeo <ngborromeo@gmail.com>
2012-10-26 16:23:50 -0700

    Add user input to accept strings

--- string-manipulation/README.md
+++ string-manipulation/README.md
@@ -10,3 +10,15 @@ result: puzzl
 
 ---
 
+#Find The Needles in the Haystack
+
+This week it's another classic string based question - to find the number of occurrences of a given String (needle) in another (haystack).
+
+Sample Input: 
+Please enter the first string representing the 'needle' > a
+Please enter the second string representing the 'haystack' > baca
+
+Sample Output:
+The number of occurrences for a is 2
+
+---
--- string-manipulation/needles_in_haystack.rb
+++ string-manipulation/needles_in_haystack.rb
@@ -1,8 +1,14 @@
-def find_needle_occurances(needle, haystack)
+def find_needle_occurrences(needle, haystack)
   
   count =  haystack.split('').count(needle)
-  puts count
+  puts "The number of occurrences for #{needle} is #{count} "
   
 end
 
-find_needle_occurances("a","baca")
+print "Please enter the first string representing the 'needle' > "
+needle = gets.chomp
+
+print "Please enter the second string representing the 'haystack' > "
+haystack = gets.chomp
+
+find_needle_occurances(needle,haystack)
