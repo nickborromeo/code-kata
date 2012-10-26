5646f2642e03a198f6638834c2343a49ec598e8a
Nick Borromeo <ngborromeo@gmail.com>
2012-10-26 13:31:26 -0700

    Implement method that will determine the twin primes is a list of natural numbers

---
+++ twin-primes/twin_primes.rb
@@ -0,0 +1,24 @@
+def twin_prime(limit)
+  
+  primes = []
+  twin_primes = []
+  non_primes = /^1?$|^(11+?)\1+$/   # regex to determine if number is not primeclear
+  
+  #Find the primes in the limit given
+  0.upto(limit) do |num|
+     primes << num unless non_primes.match(("1" * num))
+  end
+  
+  # Loop to determine the twin primes. 
+  for i in 0..(primes.length-2)
+    diff = primes[i+1] - primes[i]
+    if diff == 2
+      twin_primes << "(#{primes[i]}, #{primes[i+1]})"
+    end
+  end
+      
+  puts twin_primes
+end
+
+# Call to test the method
+twin_prime(61)
