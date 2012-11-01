1b7e2eae626a3d4cd7947d6a824b34b7dc3e9af2
Nick Borromeo <ngborromeo@gmail.com>
2012-11-01 16:27:52 -0700

    Implement bringing down of words from a source on the internet

--- string-manipulation/anagram-solver/anagram.rb
+++ string-manipulation/anagram-solver/anagram.rb
@@ -1,3 +1,11 @@
-def anagrammatize
+require 'open-uri'
 
+def anagrammatize
+  # reach out to words.txt from github project and parse it into a string
+  words = open('https://raw.github.com/rmxdave/wordchecker/master/words.txt')
+  dictionary = words.read
+  puts dictionary
+  
 end
+
+anagrammatize
