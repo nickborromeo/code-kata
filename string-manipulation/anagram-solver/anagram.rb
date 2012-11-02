2908cace0afd6236767be8d768b27fe430197c83
Nick Borromeo <ngborromeo@gmail.com>
2012-11-02 11:32:32 -0700

    Use a sorted hash implementation to find anagram solutions

--- string-manipulation/anagram-solver/anagram.rb
+++ string-manipulation/anagram-solver/anagram.rb
@@ -1,11 +1,26 @@
 require 'open-uri'
 
-def anagrammatize
-  # reach out to words.txt from github project and parse it into a string
-  words = open('https://raw.github.com/rmxdave/wordchecker/master/words.txt')
-  dictionary = words.read
-  puts dictionary
+def anagrammatize(anagram)
   
+  # create the dicitionary 
+  dictionary = create_dictionary_hash
+  
+  # look for anagram solutions
+  sorted_word = anagram.downcase.split('').sort!.join('')
+  anagram_solutions = dictionary[sorted_word]
+  
+  # display the solution to the anagrams
+  puts anagram_solutions
 end
 
-anagrammatize
+def create_dictionary_hash
+  hased_dictionary = Hash.new("")
+  # reach out to words.txt from github project and parse it into a string
+  open('https://raw.github.com/rmxdave/wordchecker/master/words.txt') do |line|
+    line.each_line {|word| hased_dictionary[word.chomp.split('').sort!.join('')] += word }
+  end
+  # return the hash of sorted words 
+  hased_dictionary
+end
+
+anagrammatize("TEST")
