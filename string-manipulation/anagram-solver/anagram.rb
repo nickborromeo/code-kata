2c1a26e1031b1fef1bd749e39a164c84a75c56ad
Nick Borromeo <ngborromeo@gmail.com>
2012-11-02 13:03:50 -0700

    Hash based implementation of the anagram solver using files and fetching from a URI

--- string-manipulation/anagram-solver/anagram.rb
+++ string-manipulation/anagram-solver/anagram.rb
@@ -1,26 +1,69 @@
 require 'open-uri'
 
-def anagrammatize(anagram)
+def anagrammatize(anagram, mode)
   
   # create the dicitionary 
-  dictionary = create_dictionary_hash
+  dictionary = HashDictionary.new
   
-  # look for anagram solutions
   sorted_word = anagram.downcase.split('').sort!.join('')
-  anagram_solutions = dictionary[sorted_word]
   
-  # display the solution to the anagrams
-  puts anagram_solutions
-end
-
-def create_dictionary_hash
-  hased_dictionary = Hash.new("")
-  # reach out to words.txt from github project and parse it into a string
-  open('https://raw.github.com/rmxdave/wordchecker/master/words.txt') do |line|
-    line.each_line {|word| hased_dictionary[word.chomp.split('').sort!.join('')] += word }
+  case mode
+  when "online"
+    online_dictionary = dictionary.create_dictionary_hash_from_web
+    puts online_dictionary[sorted_word]
+  when "offline"
+    dictionary.create_dictionary_hash_from_file unless File.exists?("./sorted_words.txt")
+    puts look_up_cached_dictionary(sorted_word)
+  else
+    puts "Not a valid choice!"
   end
-  # return the hash of sorted words 
-  hased_dictionary
+
 end
 
-anagrammatize("TEST")
+def look_up_cached_dictionary(sorted_word)
+  cached_dictionary = nil
+  File.open("./sorted_words.txt", "r") { |file| cached_dictionary = Marshal.load(file) }
+  cached_dictionary[sorted_word]
+end
+
+class HashDictionary
+  
+  attr_accessor :hashed_dictionary
+  
+  def initialize
+    @hashed_dictionary = Hash.new("")
+  end
+  
+  def create_dictionary_hash_from_web
+    
+    # reach out to words.txt from github project and parse it into a string
+    open('https://raw.github.com/rmxdave/wordchecker/master/words.txt') do |line|
+      line.each_line {|word| @hashed_dictionary[word.chomp.split('').sort!.join('')] += word }
+    end
+    # return the hash of sorted words 
+    @hashed_dictionary
+  end
+  
+  def create_dictionary_hash_from_file
+    # Create the hash from the words.txt file in the application
+    File.open("./words.txt", "r") do |file|
+      while word = file.gets
+        @hashed_dictionary[word.chomp.split('').sort!.join('')] += word
+      end
+    end
+   
+    # Serialize the hash into ta file for future use. 
+    File.open("./sorted_words.txt", "w") do |file|
+      Marshal.dump(@hashed_dictionary, file)
+    end
+  end
+  
+end  
+
+print "Please input the word to solve for anagrams: "
+word = gets.chomp
+
+print "Please input the mode that you want. online or offline: "
+mode = gets.downcase.chomp
+
+anagrammatize(word, mode)
