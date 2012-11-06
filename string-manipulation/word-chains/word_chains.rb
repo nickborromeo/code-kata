45d20448a985d69f3949a9181ba6daf3ef779532
Nick Borromeo <ngborromeo@gmail.com>
2012-11-06 15:14:24 -0800

    Save word chain into data struct. Add conditions for other cases.

--- string-manipulation/word-chains/word_chains.rb
+++ string-manipulation/word-chains/word_chains.rb
@@ -1,5 +1,6 @@
 def word_converter(start_word, end_word)
-  
+ 
+  word_chain = []
   # loop through until start word == end word
   index = 0
   while start_word != end_word
@@ -7,29 +8,33 @@ def word_converter(start_word, end_word)
     tmpword = replace_char(start_word, end_word, index)
     start_word = tmpword
     
-    puts tmpword # display the wordsrm
+    word_chain << tmpword # save the chain in an array
     
     index += 1 
     
-    index = 0 if index == start_word.length # reset index to beginning of string
+    index = 0 if index == (start_word.length > end_word.length ? start_word.length : end_word.length) # reset index to beginning of string
   end
-
+  
+  word_chain
 end
 
 def replace_char(start_word, end_word, index)
   
-  temp_word = start_word.sub(/#{start_word[index]}/, end_word[index])
+  temp_word = if end_word.length == start_word.length 
+                start_word.sub(/#{start_word[index]}/, end_word[index])
+              else
+                end_word.sub(/#{end_word[index]}/, start_word[index])
+              end
+                
   
-  # check to see if start word is a valid word
-  valid_dictionary_word?(temp_word) ? temp_word : start_word
-
+  valid_dictionary_word?(temp_word) ? temp_word : start_word # check to see if start word is a valid word
 end
 
 def valid_dictionary_word?(word)
   dictionary_word = open("./words.txt").grep(/\b#{word}\b/)
   
   dictionary_word.empty? ? false : true # return true if word is a valid word
-
 end
 
-word_converter("cat","dog")
+# Display the word chain
+puts word_converter("dog","cat")
