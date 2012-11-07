2bf2a116cf9bae8be909aa9697ded272abbad1ff
Nick Borromeo <ngborromeo@gmail.com>
2012-11-06 22:09:59 -0800

    Implement factorial using Ruby syntax

--- number-puzzles/factorial.rb
+++ number-puzzles/factorial.rb
@@ -1,11 +1,9 @@
 def factorial(n)
-  sum = 1
-  return 1 if n == 0
-  while n > 0
-    sum *= n
-    n -= 1
-  end
-  sum
+  return 1 if n == 0    # base case of 0
+  1.upto(n).inject(:*)  # solution to factorial
 end
 
-puts factorial(3)
+print "Please input number that you want to compute the factorial of: "
+num = gets.chomp.to_i
+
+puts factorial(num)
