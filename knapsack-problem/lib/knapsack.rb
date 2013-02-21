da12b51e55d6542aa846a4a458fe207f1b7dcc8d
Nick Borromeo <ngborromeo@gmail.com>
2013-02-21 13:28:42 -0800

    Implement the #max_value. TODO: Display list of items packed

--- knapsack-problem/lib/knapsack.rb
+++ knapsack-problem/lib/knapsack.rb
@@ -1,14 +1,30 @@
 class Knapsack
-  attr_accessor :items, :limit
+  attr_accessor :items
   
-  def initialize(items, limit)
+  def initialize(items)
     @items = items
-    @limit = limit
   end
 
-  def pack(items, index)
+  def max_value(index, limit)
+    item = @items[index]
+  
     if index == 0
-      items[index].weight <=  @limit ? items[index] : nil
+      if item.weight <= limit
+        return item.value
+      else
+        return 0
+      end
     end
+    
+    dont_take_branch = max_value(index-1, limit)
+
+    if item.weight > limit
+      return dont_take_branch 
+    else
+      do_take_branch = item.value + max_value(index-1, limit - item.weight)
+    end
+
+    return [dont_take_branch,do_take_branch].max
+
   end
 end
--- knapsack-problem/spec/lib/knapsack_spec.rb
+++ knapsack-problem/spec/lib/knapsack_spec.rb
@@ -2,20 +2,16 @@ require 'knapsack'
 
 describe Knapsack do
   let(:items) { [Item.new("laptop", 2, 3), Item.new("gold",1,5)] }
-  subject { Knapsack.new(items, 2) }
-  it { should respond_to(:items) }
-  it { should respond_to(:limit) }
+  let(:limit) { 2 }
 
-  describe "#pack" do
+  subject { Knapsack.new(items) }
+  it { should respond_to(:items) }
+  it { subject.max_value(1,2).should eq(5) }
+
+  describe "#max_value" do
     context "when index 0" do
-      it "should return item if weight < aw" do
-        # subject.should_receive(:pack).with(items,0).and_return(0)
-        subject.pack(items, 0).should be_an(Item)
-      end
-      it "should return nil if weight > aw" do
-        knapsack = Knapsack.new(items,1)
-        knapsack.pack(items,0).should be_nil
-      end
+      it { subject.max_value(0,2).should eq(3) }
+      it { subject.max_value(0,1).should eq(0) }
     end
   end
 end
