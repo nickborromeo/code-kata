3a4e0d8bd462ff725f30f67f48f46e1e95f26e83
Nick Borromeo <ngborromeo@gmail.com>
2013-02-25 17:04:03 -0800

    Start inital steps to manual square root algo

--- square-root/lib/square.rb
+++ square-root/lib/square.rb
@@ -1,10 +1,24 @@
 module SquareRoot 
   
-  def self.format_number(num)
+  def sqrt(number)
+    num_arr = format_number(number).split('')
+    pairs = pair_numbers(num_arr)
+    p pairs
+    square_root = ""
+    while !pairs.empty?
+      pair = pairs.pop
+      prev_num = 0
+    end
+    square_root.to_f
+  end
+  
+  private
+
+  def format_number(num)
     "%.4f" % num
   end
 
-  def self.pair_numbers(num_arr)
+  def pair_numbers(num_arr)
     pairs = []
     while !num_arr.empty?
       if num_arr[-1] == "."
--- square-root/spec/lib/square_spec.rb
+++ square-root/spec/lib/square_spec.rb
@@ -1,24 +1,29 @@
+require "spec_helper"
 require "square"
 
 describe SquareRoot do
   class TestClass
   end
 
-  let(:number) { 123.32 }
-  
-  before(:all) do
+  let(:number) {123.32}
+
+  before(:each) do
     @test = TestClass.new
     @test.extend SquareRoot
   end
 
-  describe ".format_number" do
-    it { should respond_to(:format_number).with(1).argument }
-    it { SquareRoot.format_number(3).should eq("3.0000") }
-  end
-  describe ".pair_numbers" do
-    num_arr = SquareRoot.format_number(123.32).split('')
-    it { should respond_to(:pair_numbers).with(1).argument }
-    it { SquareRoot.pair_numbers(num_arr).should eq(%w{00 32 . 23 1})}
-    it { SquareRoot.pair_numbers(num_arr).should be_an(Array) }
+  # Already tested the private methods
+  #
+  # describe "#format_number" do
+  #   it { @test.format_number,3).should eq("3.0000") }
+  # end
+  # describe "#pair_numbers" do
+  #   num_arr = @test.format_number(123.32).split('')
+  #   it { @test.pair_numbers(num_arr).should eq(%w{00 32 . 23 1}) }
+  #   it { @test.pair_numbers(num_arr).should be_an(Array) }
+  # end
+
+  describe "#sqrt" do
+    it { @test.sqrt(number).should be_a(Float)}
   end
 end
